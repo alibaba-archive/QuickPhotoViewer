d0dbc6cf556b6ed3ef2733e384a984ff3100a55c
Xin Hong <hongxinhope@gmail.com>
2017-03-15 13:39:00 +0800

    Initial Commit

---
+++ QuickPhotoViewer/QuickPhotoViewer.swift
@@ -0,0 +1,9 @@
+//
+//  QuickPhotoViewer.swift
+//  QuickPhotoViewer
+//
+//  Created by 洪鑫 on 2017/3/15.
+//  Copyright © 2017年 Teambition. All rights reserved.
+//
+
+import UIKit
---
+++ QuickPhotoViewerExample/ExampleViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ExampleViewController.swift
+//  QuickPhotoViewerExample
+//
+//  Created by 洪鑫 on 2017/3/15.
+//  Copyright © 2017年 Teambition. All rights reserved.
+//
+
+import UIKit
+
+class ExampleViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
